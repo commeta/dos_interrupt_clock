1eac47678bc996611927b1ae043d5eead89c5531
Евгений <40506054+commeta@users.noreply.github.com>
2024-04-06 12:18:51 +0300

    Create cutter_disasm_clockoff_com.asm

---
+++ cutter_disasm_clockoff_com.asm
@@ -0,0 +1,27 @@
+;hex
+;b8c20dcd210653b86135cd21061f535ab81c25cd210633c08ec0bf1704268a0524ef26880507580783e8028bf8268b05488ec0b449cd21730804308ad0b402cd21b8004ccd21
+fcn.00000000();
+0x00000000      mov     eax, 0x21cd0dc2
+0x00000005      invalid
+0x00000006      push    rbx
+0x00000007      mov     eax, 0x21cd3561
+0x0000000c      invalid
+0x0000000d      invalid
+0x0000000e      push    rbx
+0x0000000f      pop     rdx
+0x00000010      mov     eax, 0x21cd251c
+0x00000015      invalid
+0x00000016      xor     eax, eax
+0x00000018      mov     es, eax
+0x0000001a      mov     edi, 0x8a260417
+0x0000001f      add     eax, 0x8826ef24
+0x00000024      add     eax, 0x83075807
+0x00000029      call    0x26f88b30
+0x0000002e      mov     eax, dword [0xffffffffb4c08e7c]
+0x00000034      int     0x21
+0x00000037      jae     0x41
+0x00000039      add     al, 0x30   ; 48
+0x0000003b      mov     dl, al
+0x0000003d      mov     ah, 2
+0x0000003f      int     0x21
+0x00000041      mov     eax, 0x21cd4c00
